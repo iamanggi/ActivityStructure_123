eea671ef1c2d55765295fedf9dbfb5e4739a2141
iamanggi <anggipuspita452@gmail.com>
2023-04-03 10:47:16 +0700

    menampilkan cout dari mahasiswa

--- ActivityStructure2_123/ActivityStructure2_123.cpp
+++ ActivityStructure2_123/ActivityStructure2_123.cpp
@@ -14,3 +14,19 @@ struct mahasiswa
 	AlamatDetail alamat;
 	int Umur;
 };
+
+int main() {
+	mahasiswa mhs;
+
+	cout << "masukan NIM : ";
+	cin >> mhs.Nim;
+	cout << "masukan nama : ";
+	cin >> mhs.nama;
+	cout << "alamat : ";
+	cout << "\tDesa : ";
+	cin >> mhs.alamat.desa;
+	cout << "\tKota : ";
+	cin >> mhs.alamat.kota;
+	cout << "masukan umur : ";
+	cin >> mhs.Umur;
+}
