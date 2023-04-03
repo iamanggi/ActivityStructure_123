4e23cfcf6b6552fa65180af3dc5ce97115c4c6da
iamanggi <anggipuspita452@gmail.com>
2023-04-03 11:17:45 +0700

    menampilkan prosedur

--- ActivityStructure3_123/ActivityStructure3_123.cpp
+++ ActivityStructure3_123/ActivityStructure3_123.cpp
@@ -10,8 +10,27 @@ struct AlamatDetail
 
 struct mahasiswa
 {
-	char NIM[20];
+	char NIM[12];
 	char nama[20];
-	AlamatDetail alamat[25];
+	AlamatDetail alamat;
 	int umur;
 };
+
+int main() {
+	mahasiswa mhs[3];
+	
+	for (int i = 0; i < 3; i++) {
+		cout << "masukan NIM : ";
+		cin.getline(mhs[i].NIM,12);
+		cout << "masukan nama : ";
+		cin.getline(mhs[i].nama, 20);
+		cout << "alamat : ";
+		cout << "\n\tMasukan Desa : ";
+		cin.getline(mhs[i].alamat.Desa,25);
+		cout << "\n\tMasukan Kota : ";
+		cin.getline(mhs[i].alamat.Kota, 25);
+		cout << "\n masukan umur : ";
+		cin.ignore(1, '\n');
+
+	}
+}
